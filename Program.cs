508524536cbe82999b57bb83007789e8c9b06c18
Maria Julia Cavalcanti <majucavalcantix@gmail.com>
2024-03-22 20:51:21 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//@niallivmi
+using System;
+
+namespace Pgc1Aula02Ex04
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            float va, vr;
+
+            Console.Write("Digite o valor aplicado: ");
+            va = float.Parse(Console.ReadLine());   
+
+            Console.Write("Digite o valor resgatado: ");
+            vr = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("O valor percentual da aplicação financeira é de: "+((vr-va)/va)*100);
+        }
+    }
+}
